2e8e9b79f809c74497b22f567bdb5e0965571c23
Martin Junk <junk@logio.cz>
2017-07-14 19:57:06 +0200

    Fixed bash script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#/bin/bash -e
+#!/bin/bash -e
 cp -n config-template/newrelic.template.json config/newrelic.json
 cp -n config-template/plugin.template.json config/plugin.json
 cp -n config-template/* config/
